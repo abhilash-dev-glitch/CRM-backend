b9cf6f61d2e4d026161598b595853f2257959265
Abhilash chandra <abhiashchandra26@gmail.com>
2025-09-17 22:03:34 +0530

    server.js updated

--- server.js
+++ server.js
@@ -53,4 +53,8 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ message: err.message || 'Server Error' });
 });
 
-module.exports = app;
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
